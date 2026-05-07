79f78d8e8455ff784ef4ded31da34dd34f1ec9ef
buholoko <eduardo12321312@gmail.com>
2026-05-07 03:26:01 +0200

    menu boton y portada
    Co-Authored-By: Elenalluq <235091475+Elenalluq@users.noreply.github.com>

--- proyecto juego/Assets/Repaso2EVA/Scripts/menu/MenuManager.cs
+++ proyecto juego/Assets/Repaso2EVA/Scripts/menu/MenuManager.cs
@@ -9,4 +9,12 @@ public class MenuManager : MonoBehaviour
         // Reemplaza "Nivel1" por el nombre exacto de la escena a la que quieres ir
         SceneManager.LoadScene("MainLevel");
     }
+
+    public void Salir()
+        {
+            Debug.Log("Saliendo del juego...");
+        // Esto cerrará la aplicación. En el editor no hará nada, pero en un build sí.
+        Application.Quit();
+    }
+
 }
